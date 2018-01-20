c6cea28bc7bd02d7ef76182e432535d033808fe5
Romain Beucher <rbeucher@unimelb.edu.au>
2018-01-20 19:28:33 +1100

    Convert both swarm and mesh variable to ascii

--- UWGeodynamics/utilities/UWtoAscii.py
+++ UWGeodynamics/utilities/UWtoAscii.py
@@ -20,20 +20,27 @@ def convert_to_ascii(file_input, file_output=None):
     geometry = grid.find("Geometry")
     attribute = grid.find("Attribute")
 
-    file_geometry = geometry[0].text
-    file_attribute = attribute[0].text
+    if grid.get("Name") == "swarm":
 
-    f = h5py.File(os.path.join(root_dir, file_geometry.split(":")[0]), "r")
-    geometry = f["/data"]
+        file_geometry = geometry[0].text
+        f = h5py.File(os.path.join(root_dir, file_geometry.split(":")[0]), "r")
+        geometry = f["/data"]
 
-    f = h5py.File(os.path.join(root_dir, file_attribute.split(":")[0]), "r")
+    elif grid.get("Name") == "FEM_Mesh_mesh":
+
+        geometryFile = geometry[0][0][1].text
+        f = h5py.File(os.path.join(root_dir, geometryFile.split(":")[0]), "r")
+        geometry = f["/vertices"]
+
+    attributesFile = attribute[0].text
+    f = h5py.File(os.path.join(root_dir, attributesFile.split(":")[0]), "r")
     data = f["/data"]
 
     new = np.zeros((geometry.value.shape[0], geometry.value.shape[1] + 1))
     new[:, :-1] = geometry.value
     new[:, -1] = data.value.ravel()
 
-    if not file_output:
+    if file_output is None or file_output == "None":
         file_output = os.path.split(file_input)[-1].split(".")[0] + ".asc"
 
     np.savetxt(file_output, new)
@@ -45,7 +52,7 @@ def main():
                    for import into MOVE/PETREL"""
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('-i', '--input', help='input file', required=True)
-    parser.add_argument('-o', '--output', help='output file', required=True)
+    parser.add_argument('-o', '--output', help='output file', required=False)
     args = parser.parse_args()
 
     file_input = str(args.input)
