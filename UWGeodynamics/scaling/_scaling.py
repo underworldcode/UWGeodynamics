8cbd73860c9e8ff0b10b30f58468b4ad67448a59
Romain Beucher <rbeucher@unimelb.edu.au>
2018-01-27 08:44:26 +1100

    cleaning

--- UWGeodynamics/scaling/_scaling.py
+++ UWGeodynamics/scaling/_scaling.py
@@ -2,12 +2,8 @@
 # Romain BEUCHER, December 2016
 
 import underworld as uw
-import h5py
-import numpy as np
-import os
-from _coefficients import COEFFICIENTS as scaling
 from _coefficients import u
-from mpi4py import MPI
+
 
 def nonDimensionalize(dimValue):
     """
@@ -27,18 +23,18 @@ def nonDimensionalize(dimValue):
     surfaceTemp = 0. * u.degK
     baseModelTemp = 1330. * u.degC
     baseCrustTemp = 550. * u.degC
-    
+
     KL_meters = model_height
     KT_seconds = KL_meters / half_rate
     KM_kilograms = refViscosity * KL_meters * KT_seconds
     Kt_degrees = (baseModelTemp - surfaceTemp)
     K_substance = 1. * u.mole
 
-    sca.scaling["[time]"] = KT_seconds
-    sca.scaling["[length]"] = KL_meters 
-    sca.scaling["[mass]"] = KM_kilograms 
-    sca.scaling["[temperature]"] = Kt_degrees
-    sca.scaling["[substance]"] -= K_substance
+    sca.scaling_coefficients["[time]"] = KT_seconds
+    sca.scaling_coefficients["[length]"] = KL_meters
+    sca.scaling_coefficients["[mass]"] = KM_kilograms
+    sca.scaling_coefficients["[temperature]"] = Kt_degrees
+    sca.scaling_coefficients["[substance]"] -= K_substance
 
     # Get a scaled value:
     gravity = nonDimensionalize(9.81 * u.meter / u.second**2)
@@ -51,85 +47,87 @@ def nonDimensionalize(dimValue):
             return dimValue
     except AttributeError:
         return dimValue
-    
+
     dimValue = dimValue.to_base_units()
-    
+
     length = scaling["[length]"]
     time = scaling["[time]"]
     mass = scaling["[mass]"]
     temperature = scaling["[temperature]"]
     substance = scaling["[substance]"]
-    
+
     length = length.to_base_units()
     time = time.to_base_units()
     mass = mass.to_base_units()
     temperature = temperature.to_base_units()
     substance = substance.to_base_units()
-    
-    @u.check('[length]','[time]', '[mass]', '[temperature]', '[substance]')
+
+    @u.check('[length]', '[time]', '[mass]', '[temperature]', '[substance]')
     def check(length, time, mass, temperature, substance):
         return
-    
+
     check(length, time, mass, temperature, substance)
 
     # Get dimensionality
     dlength = dimValue.dimensionality['[length]']
-    dtime   = dimValue.dimensionality['[time]']
-    dmass   = dimValue.dimensionality['[mass]']
-    dtemp   = dimValue.dimensionality['[temperature]']
-    dsubstance   = dimValue.dimensionality['[substance]']
+    dtime = dimValue.dimensionality['[time]']
+    dmass = dimValue.dimensionality['[mass]']
+    dtemp = dimValue.dimensionality['[temperature]']
+    dsubstance = dimValue.dimensionality['[substance]']
     factor = (length**(-dlength) *
               time**(-dtime) *
               mass**(-dmass) *
-              temperature**(-dtemp)*
+              temperature**(-dtemp) *
               substance**(-dsubstance))
-    
+
     dimValue *= factor
-    
-    if dimValue.unitless :
+
+    if dimValue.unitless:
         return dimValue.magnitude
     else:
         raise ValueError('Dimension Error')
 
+
 def Dimensionalize(Value, units):
-   
+
     global scaling
 
     unit = (1.0 * units).to_base_units()
-    
+
     length = scaling["[length]"]
     time = scaling["[time]"]
     mass = scaling["[mass]"]
     temperature = scaling["[temperature]"]
     substance = scaling["[substance]"]
-    
+
     length = length.to_base_units()
     time = time.to_base_units()
     mass = mass.to_base_units()
     temperature = temperature.to_base_units()
     substance = substance.to_base_units()
-    
-    @u.check('[length]','[time]', '[mass]', '[temperature]', '[substance]')
+
+    @u.check('[length]', '[time]', '[mass]', '[temperature]', '[substance]')
     def check(length, time, mass, temperature, substance):
         return
-    
+
     # Check that the scaling parameters have the correct dimensions
     check(length, time, mass, temperature, substance)
-    
+
     # Get dimensionality
     dlength = unit.dimensionality['[length]']
-    dtime   = unit.dimensionality['[time]']
-    dmass   = unit.dimensionality['[mass]']
-    dtemp   = unit.dimensionality['[temperature]']
-    dsubstance   = unit.dimensionality['[substance]']
+    dtime = unit.dimensionality['[time]']
+    dmass = unit.dimensionality['[mass]']
+    dtemp = unit.dimensionality['[temperature]']
+    dsubstance = unit.dimensionality['[substance]']
     factor = (length**(dlength) *
               time**(dtime) *
               mass**(dmass) *
-              temperature**(dtemp)*
-              substance**(dsubstance))    
-    
-    if (isinstance(Value, uw.mesh._meshvariable.MeshVariable) or 
-       isinstance(Value, uw.swarm._swarmvariable.SwarmVariable)) :
+              temperature**(dtemp) *
+              substance**(dsubstance))
+
+    if (isinstance(Value, uw.mesh._meshvariable.MeshVariable) or
+       isinstance(Value, uw.swarm._swarmvariable.SwarmVariable)):
+
         tempVar = Value.copy()
         tempVar.data[...] = (Value.data[...] * factor).to(units)
         return tempVar
