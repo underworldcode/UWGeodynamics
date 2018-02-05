3fe65f7f42db78d9b83fe010a94afbb1eff28fea
Romain Beucher <rbeucher@unimelb.edu.au>
2018-02-05 11:32:32 +1100

    Add pyshp as a dependency

--- setup.py
+++ setup.py
@@ -125,7 +125,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pint', 'numpy', 'scipy'],  # Optional
+    install_requires=['pint', 'numpy', 'scipy', 'pyshp'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
