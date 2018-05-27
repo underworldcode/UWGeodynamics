underworldcode/UWGeodynamics

commit e7a49f03e43fc7d04d97a79f00134c730a595dca

Add option to set boundary orders

UWGeodynamics/_velocity_boundaries.py
@@ -23,7 +23,8 @@ class VelocityBCs(object):
     """ Class to define the mechanical boundary conditions """
 
     def __init__(self, Model, left=None, right=None, top=None, bottom=None,
-                 front=None, back=None, indexSets=None):
+                 front=None, back=None, indexSets=None,
+                 order_wall_conditions=None):
         """ Defines mechanical boundary conditions
 
         The type of conditions is determined through the units used do define
@@ -92,6 +93,41 @@ class VelocityBCs(object):
         self.neumann_indices = []
 
 
+        if self.Model.mesh.dim == 2:
+            self._wall_indexSets = {"bottom": (self.bottom,
+                                               self.Model._bottom_wall),
+                                    "top": (self.top,
+                                            self.Model._top_wall),
+                                    "left": (self.left,
+                                             self.Model._left_wall),
+                                    "right": (self.right,
+                                              self.Model._right_wall)}
+            if order_wall_conditions:
+                if len(order_wall_conditions) <= 5:
+                    self.order_wall_conditions = order_wall_conditions
+            else:
+                self.order_wall_conditions = ["bottom", "top", "left", "right"]
+
+        if self.Model.mesh.dim == 3:
+            self._wall_indexSets = {"bottom": (self.bottom,
+                                               self.Model._bottom_wall),
+                                    "top": (self.top,
+                                            self.Model._top_wall),
+                                    "left": (self.left,
+                                             self.Model._left_wall),
+                                    "right": (self.right,
+                                              self.Model._right_wall),
+                                    "front": (self.front,
+                                              self.Model._front_wall),
+                                    "back": (self.back,
+                                             self.Model._back_wall)}
+            if order_wall_conditions:
+                if len(order_wall_conditions) <= 7:
+                    self.order_wall_conditions = order_wall_conditions
+            else:
+                self.order_wall_conditions = ["bottom", "top", "front", "back",
+                                              "left", "right"]
+
     def __getitem__(self, name):
         return self.__dict__[name]
 
@@ -110,6 +146,32 @@ class VelocityBCs(object):
         if not nodes:
             return
 
+        # Special case (Bottom LecodeIsostasy)
+        if (isinstance(condition, LecodeIsostasy) and
+            nodes ==  self.Model._bottom_wall):
+
+            # Apply support condition
+            self.Model._isostasy = self.bottom
+            self.Model._isostasy.mesh = self.Model.mesh
+            self.Model._isostasy.swarm = self.Model.swarm
+            self.Model._isostasy._mesh_advector = self.Model._advector
+            self.Model._isostasy.velocityField = self.Model.velocityField
+            self.Model._isostasy.boundariesField = self.Model.boundariesField
+            self.Model._isostasy.materialIndexField = self.Model.materialField
+            self.Model._isostasy._densityFn = self.Model._densityFn
+            vertical_walls_conditions = {
+                "left": self.left,
+                "right": self.right,
+                "front": self.front,
+                "back": self.back
+            }
+            self.Model._isostasy.vertical_walls_conditions = (
+                vertical_walls_conditions)
+            self.dirichlet_indices[-1] += self.Model._bottom_wall
+            return
+        else:
+            self.Model._isostasy = None
+
         # Expect a list or tuple of dimension mesh.dim.
         # Check that the domain actually contains some boundary nodes
         # (nodes is not None)
@@ -126,7 +188,6 @@ class VelocityBCs(object):
                         func.evaluate(
                             self.Model.mesh.data[nodes.data])[:, dim])
                     self.dirichlet_indices[dim] += nodes
-                    continue
 
                 # Scalar condition
                 if isinstance(condition[dim], (u.Quantity, float, int)):
@@ -196,7 +257,7 @@ class VelocityBCs(object):
         """
 
         Model = self.Model
-        Model.boundariesField.data[...] = 0
+        Model.boundariesField.data[...] = np.random.random(Model.boundariesField.data.shape)
 
         # Reinitialise neumnann and dirichlet condition
         self.dirichlet_indices = []
@@ -206,40 +267,9 @@ class VelocityBCs(object):
             self.dirichlet_indices.append(Model.mesh.specialSets["Empty"])
             self.neumann_indices.append(Model.mesh.specialSets["Empty"])
 
-        # Apply condition to the bottom first
-        if isinstance(self.bottom, LecodeIsostasy):
-            # Apply support condition
-            Model._isostasy = self.bottom
-            Model._isostasy.mesh = Model.mesh
-            Model._isostasy.swarm = Model.swarm
-            Model._isostasy._mesh_advector = Model._advector
-            Model._isostasy.velocityField = Model.velocityField
-            Model._isostasy.materialIndexField = Model.materialField
-            Model._isostasy._densityFn = Model._densityFn
-            vertical_walls_conditions = {
-                "left": self.left,
-                "right": self.right,
-                "front": self.front,
-                "back": self.back
-            }
-            Model._isostasy.vertical_walls_conditions = (
-                vertical_walls_conditions)
-            self.dirichlet_indices[-1] += Model._bottom_wall
-        else:
-            Model._isostasy = None
-            self.apply_condition_nodes(self.bottom, Model._bottom_wall)
-
-        # Apply condition at the top.
-        self.apply_condition_nodes(self.top, Model._top_wall)
-
-        # Apply conditions to each wall.
-        self.apply_condition_nodes(self.left, Model._left_wall)
-        self.apply_condition_nodes(self.right, Model._right_wall)
-        self.apply_condition_nodes(self.indexSets, self.indexSets)
-
-        if Model.mesh.dim > 2:
-            self.apply_condition_nodes(self.front, Model._front_wall)
-            self.apply_condition_nodes(self.back, Model._back_wall)
+        for set_ in self.order_wall_conditions:
+            (condition, nodes) = self._wall_indexSets[set_]
+            self.apply_condition_nodes(condition, nodes)
 
         conditions = []
 

commit 38b0f2dfa9cdae46b062d0d9bda0c258002bf759

Fix bug introduced in e7a49f03 that disabled LecodeIsostasy

UWGeodynamics/_velocity_boundaries.py
@@ -176,8 +176,6 @@ class VelocityBCs(object):
                 vertical_walls_conditions)
             self.dirichlet_indices[-1] += self.Model._bottom_wall
             return
-        else:
-            self.Model._isostasy = None
 
         #if isinstance(condition, MovingWall):
         #    condition.wall = nodes