af22aafaa86b9d3377552a736fa198b25bb8f796
arijitlaik <arijit.netcom@gmail.com>
2018-07-12 18:20:59 +0000

    inital viscoityfn2 commit for testing

---
+++ UWGeodynamics/version.py
@@ -0,0 +1,12 @@
+
+# THIS FILE IS GENERATED FROM UWGeodynamics SETUP.PY
+#
+underworld_version = '2.6.0-dev'
+short_version = '0.5.0'
+version = '0.5.0'
+full_version = '0.5.0-dev-64b24e0(development)'
+git_revision = '64b24e0317f72ad818e1065f0d6a4a387533f196'
+release = False
+
+if not release:
+    version = full_version
