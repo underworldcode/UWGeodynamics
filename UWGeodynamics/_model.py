28c5c2cbabc9f16e962e043e8712f4f3ed2f675a
Arijit Laik <arijit@MB-036387.clients.vu.nl>
2018-09-28 16:10:58 +0200

    fx

--- UWGeodynamics/_model.py
+++ UWGeodynamics/_model.py
@@ -373,7 +373,7 @@ class Model(Material):
         if not os.path.exists(restartDir):
             return
 
-        if step is True:
+        if step is True and not isinstance(step, int):
             # Look for the last saved timestep
             indices = [int(os.path.splitext(filename)[0].split("-")[-1])
                        for filename in os.listdir(restartDir) if "-" in
