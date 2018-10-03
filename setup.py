32a22706e3ca4158171a9ed152e476ea63102e1b
Romain Beucher <rbeucher@unimelb.edu.au>
2018-10-03 18:11:43 +0200

    Fix

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from os import path
 
 MAJOR               = 0
 MINOR               = 9
-MICRO               = 2
+MICRO               = 3
 ISRELEASED          = False
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
