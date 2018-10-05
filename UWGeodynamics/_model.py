7ecd7adf7b760edc41477db9e9585400d2719357
Arijit Laik <arijit.netcom@gmail.com>
2018-10-05 13:18:26 +0200

    correct Docstring

--- UWGeodynamics/_model.py
+++ UWGeodynamics/_model.py
@@ -526,7 +526,7 @@ class Model(Material):
 
     @property
     def projTimeField(self):
-        """ Plastic Strain Field projected on the mesh """
+        """ Time Field projected on the mesh """
         self._timeFieldProjector.solve()
         return self._projTimeField
 
