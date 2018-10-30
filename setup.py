23d46327e17eb5310c127e042dfc36fd6cca69a8
Romain Beucher <rbeucher@unimelb.edu.au>
2018-10-30 12:18:01 +1100

    Patch

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from os import path
 
 MAJOR               = 0
 MINOR               = 9
-MICRO               = 4
+MICRO               = 6
 ISRELEASED          = False
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
