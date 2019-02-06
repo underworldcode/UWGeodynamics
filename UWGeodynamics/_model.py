d169b794d677a9b723e0be75cbe36518c808d301
Romain Beucher <romain.beucher@unimelb.edu.au>
2019-02-06 16:32:51 +1100

    Do not round the projMaterialField

--- UWGeodynamics/_model.py
+++ UWGeodynamics/_model.py
@@ -396,9 +396,6 @@ class Model(Material):
     def projMaterialField(self):
         """ Material field projected on the mesh """
         self._materialFieldProjector.solve()
-        self._projMaterialField.data[:] = np.rint(
-            self._projMaterialField.data[:]
-        )
         return self._projMaterialField
 
     @property
