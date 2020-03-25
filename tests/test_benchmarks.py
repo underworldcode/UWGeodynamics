d97db37b6bdd75bf17c5fb8ab025e19a19e57a14
Romain Beucher <rbeucher@thermo-geology.fr>
2020-03-26 10:22:54 +1100

    Fix benchmarks and test benchmarks

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -1,25 +1,20 @@
 from .utils import _notebook_run
 
 
-#def test_slab_detachment_benchmark():
-#    _notebook_run("benchmarks/1_12_Slab_Detachment_Benchmark.ipynb")
-#
-#
-#def test_indentor_benchmark():
-#    _notebook_run("benchmarks/1_22_Indentor_Benchmark.ipynb")
-#
-#
-#def test_self_subduction_case1():
-#    _notebook_run("benchmarks/2D_Self_Subduction_Case1.ipynb")
-#
-#
-#def test_self_subduction_case2():
-#    _notebook_run("benchmarks/2D_Self_Subduction_Case2.ipynb")
-#
-#
-#def test_brick_compression():
-#    _notebook_run("benchmarks/Kaus_BrickBenchmark-Compression.ipynb")
-#
-#
-#def test_brick_extension():
-#    _notebook_run("benchmarks/Kaus_BrickBenchmark_Extension.ipynb")
+def test_slab_detachment_benchmark():
+    _notebook_run("benchmarks/1_12_Slab_Detachment_Benchmark.ipynb")
+
+def test_indentor_benchmark():
+    _notebook_run("benchmarks/1_22_Indentor_Benchmark.ipynb")
+
+def test_self_subduction_case1():
+    _notebook_run("benchmarks/2D_Self_Subduction_Case1.ipynb")
+
+def test_self_subduction_case2():
+    _notebook_run("benchmarks/2D_Self_Subduction_Case2.ipynb")
+
+def test_brick_compression():
+    _notebook_run("benchmarks/Kaus_BrickBenchmark-Compression.ipynb")
+
+def test_brick_extension():
+    _notebook_run("benchmarks/Kaus_BrickBenchmark_Extension.ipynb")
