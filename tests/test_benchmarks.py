ab94f7a7b95d31c96fd4ba0f844570ca5a644e58
Romain Beucher <rbeucher@thermo-geology.fr>
2020-06-23 05:43:55 +0000

    cleaning

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -2,19 +2,19 @@ from .utils import _notebook_run
 
 
 def test_slab_detachment_benchmark():
-    _notebook_run("docs/benchmarks/1_12_Slab_Detachment_Benchmark.ipynb")
+    _notebook_run("benchmarks/1_12_Slab_Detachment_Benchmark.ipynb")
 
 def test_indentor_benchmark():
-    _notebook_run("docs/benchmarks/1_22_Indentor_Benchmark.ipynb")
+    _notebook_run("benchmarks/1_22_Indentor_Benchmark.ipynb")
 
 def test_self_subduction_case1():
-    _notebook_run("docs/benchmarks/2D_Self_Subduction_Case1.ipynb")
+    _notebook_run("benchmarks/2D_Self_Subduction_Case1.ipynb")
 
 def test_self_subduction_case2():
-    _notebook_run("docs/benchmarks/2D_Self_Subduction_Case2.ipynb")
+    _notebook_run("benchmarks/2D_Self_Subduction_Case2.ipynb")
 
 def test_brick_compression():
-    _notebook_run("docs/benchmarks/Kaus_BrickBenchmark-Compression.ipynb")
+    _notebook_run("benchmarks/Kaus_BrickBenchmark-Compression.ipynb")
 
 def test_brick_extension():
-    _notebook_run("docs/benchmarks/Kaus_BrickBenchmark_Extension.ipynb")
+    _notebook_run("benchmarks/Kaus_BrickBenchmark_Extension.ipynb")
