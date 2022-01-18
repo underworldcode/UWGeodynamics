2660d24f3624edad7ffbd2215c76428ab1ec0ed5
Romain Beucher <romain.beucher@anu.edu.au>
2022-01-19 09:46:11 +1100

    Trigger docker image build on dispatch

---
+++ UWGeodynamics/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM UWGeodynamics SETUP.PY
+#
+short_version = '2.12.0'
+version = '2.12.0'
+full_version = '2.12.0-dev-885589a(development)'
+git_revision = '885589ab24d4ee72e692d80d4b2206c42386b52b'
+release = False
+
+if not release:
+    version = full_version
