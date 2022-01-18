f135564a63242e67c5ee47938dc8932c254e3a88
Romain Beucher <romain.beucher@anu.edu.au>
2022-01-19 09:14:53 +1100

    Update Notebook tests

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -1,20 +1,12 @@
+# test_benchmark.py
+
 from .utils import _notebook_run
+import pytest
+import glob
+import ntpath
 
+scripts = [pytest.param(path, id=ntpath.basename(path)) for path in sorted(glob.glob("docs/benchmarks/*.ipynb"))]
 
-def test_slab_detachment_benchmark():
-    _notebook_run("benchmarks/1_12_Slab_Detachment_Benchmark.ipynb")
-
-def test_indentor_benchmark():
-    _notebook_run("benchmarks/1_22_Indentor_Benchmark.ipynb")
-
-def test_self_subduction_case1():
-    _notebook_run("benchmarks/2D_Self_Subduction_Case1.ipynb")
-
-def test_self_subduction_case2():
-    _notebook_run("benchmarks/2D_Self_Subduction_Case2.ipynb")
-
-def test_brick_compression():
-    _notebook_run("benchmarks/Kaus_BrickBenchmark-Compression.ipynb")
-
-def test_brick_extension():
-    _notebook_run("benchmarks/Kaus_BrickBenchmark_Extension.ipynb")
+@pytest.mark.parametrize('script', scripts)
+def test_script_execution(script):
+    _notebook_run(script)
--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -1,82 +1,14 @@
+# test_tutorials.py
+
 from .utils import _notebook_run
-from .utils import _notebook_run_parallel
+import pytest
+import glob
+import ntpath
 
-def test_tutorial1():
-   _notebook_run("tutorials/Tutorial_1_ThermoMechanical_Model.ipynb")
+scripts = [pytest.param(path, id=ntpath.basename(path)) for path in sorted(glob.glob("docs/tutorials/*.ipynb"))]
 
-
-def test_tutorial1_parallel():
-   _notebook_run_parallel("tutorials/Tutorial_1_ThermoMechanical_Model.ipynb")
-
-
-def test_tutorial2():
-   _notebook_run("tutorials/Tutorial_2_Melt.ipynb")
-
-def test_tutorial2_parallel():
-   _notebook_run_parallel("tutorials/Tutorial_2_Melt.ipynb")
-
-
-def test_tutorial3():
-   _notebook_run("tutorials/Tutorial_3_SandboxExtension_static_mesh.ipynb")
-
-def test_tutorial3_parallel():
-   _notebook_run_parallel("tutorials/Tutorial_3_SandboxExtension_static_mesh.ipynb")
-
-
-def test_tutorial3B():
-   _notebook_run("tutorials/Tutorial_3B_SandboxExtension_deform_mesh.ipynb")
-
-def test_tutorial3B_parallel():
-   _notebook_run_parallel("tutorials/Tutorial_3B_SandboxExtension_deform_mesh.ipynb")
-
-
-def test_tutorial4():
-   _notebook_run("tutorials/Tutorial_4_NumericalSandboxCompression.ipynb")
-
-def test_tutorial4_parallel():
-   _notebook_run_parallel("tutorials/Tutorial_4_NumericalSandboxCompression.ipynb")
-
-
-def test_tutorial5():
-   _notebook_run("tutorials/Tutorial_5_Convergence_Model.ipynb")
-
-
-def test_tutorial5_parallel():
-   _notebook_run_parallel("tutorials/Tutorial_5_Convergence_Model.ipynb")
-
-
-def test_tutorial6():
-   _notebook_run("tutorials/Tutorial_6_Simple_Surface_Processes.ipynb")
-
-def test_tutorial6_parallel():
-   _notebook_run_parallel("tutorials/Tutorial_6_Simple_Surface_Processes.ipynb")
-
-def test_tutorial7():
-   _notebook_run("tutorials/Tutorial_7_3D_Lithospheric_Model.ipynb")
-
-
-def test_tutorial7_parallel():
-   _notebook_run_parallel("tutorials/Tutorial_7_3D_Lithospheric_Model.ipynb")
-
-
-def test_tutorial8():
-   _notebook_run("tutorials/Tutorial_8_Subduction_ViscoElastic.ipynb")
-
-
-def test_tutorial8_parallel():
-   _notebook_run_parallel("tutorials/Tutorial_8_Subduction_ViscoElastic.ipynb")
-
-
-def test_tutorial9():
-   _notebook_run("tutorials/Tutorial_9_passive_margins.ipynb")
-
-def test_tutorial9_parallel():
-   _notebook_run_parallel("tutorials/Tutorial_9_passive_margins.ipynb")
-
-def test_tutorial10():
-   _notebook_run("tutorials/Tutorial_10_Thrust_Wedges.ipynb")
-
-def test_tutorial10_parallel():
-   _notebook_run_parallel("tutorials/Tutorial_10_Thrust_Wedges.ipynb")
+@pytest.mark.parametrize('script', scripts)
+def test_script_execution(script):
+    _notebook_run(script)
 
 
